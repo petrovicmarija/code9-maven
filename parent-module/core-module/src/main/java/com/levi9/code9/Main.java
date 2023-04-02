8e63ac28a567eee4808b0dbd778c7961f18a7592
Marija Petrovic <marijap@protonmail.com>
2023-04-02 13:33:04 +0200

    Add example with calculator

--- parent-module/core-module/src/main/java/com/levi9/code9/Main.java
+++ parent-module/core-module/src/main/java/com/levi9/code9/Main.java
@@ -3,5 +3,9 @@ package com.levi9.code9;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world from core module!");
+
+        Calculator calculator = new Calculator();
+        calculator.add(2, 5);
     }
+
 }
---
+++ parent-module/src/main/java/com/levi9/code9/Calculator.java
@@ -0,0 +1,8 @@
+package com.levi9.code9;
+
+public class Calculator {
+
+    public void add(int a, int b) {
+        System.out.println("Result is: " + (a + b));
+    }
+}
